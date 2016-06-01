caf0270f8d94ee65ad4643e8bd4fc5f9b072917e
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-06-01 08:28:50 +0100

    Updated media

--- app/scripts/mediamap.js
+++ app/scripts/mediamap.js
@@ -4,12 +4,78 @@ var mediaMapJson = {
 			"voice": {
 				"john": {},
 				"ann": {
+					"a": [
+						{
+							"word": "airplane",
+							"spoken": "airplane.mp3",
+							"effect": "airplane.mp3",
+							"picture": "airplane.jpg"
+						}
+					],
+					"b": [
+						{
+							"word": "baby",
+							"spoken": "baby.mp3",
+							"effect": "baby.mp3",
+							"picture": "baby.jpg"
+						},
+						{
+							"word": "ball",
+							"spoken": "ball.mp3",
+							"effect": "ball.mp3",
+							"picture": "ball.jpg"
+						},
+						{
+							"word": "bear",
+							"spoken": "bear.mp3",
+							"effect": "bear.mp3",
+							"picture": "bear.jpg"
+						},
+						{
+							"word": "bee",
+							"spoken": "bee.mp3",
+							"effect": "bee.mp3",
+							"picture": "bee.jpg"
+						}
+					],
+					"c": [
+						{
+							"word": "car",
+							"spoken": "car.mp3",
+							"effect": "car.mp3",
+							"picture": "car.jpg"
+						}
+					],
+					"d": [
+						{
+							"word": "dinossaur",
+							"spoken": "dinossaur.mp3",
+							"effect": "dinossaur.mp3",
+							"picture": "dinossaur.jpg"
+						}
+					],
 					"e": [
 						{
 							"word": "elephant",
 							"spoken": "elephant.mp3",
 							"effect": "elephant.mp3",
 							"picture": "elephant.jpg"
+						}
+					],
+					"l": [
+						{
+							"word": "lion",
+							"spoken": "lion.mp3",
+							"effect": "lion.mp3",
+							"picture": "lion.jpg"
+						}
+					],
+					"t": [
+						{
+							"word": "tiger",
+							"spoken": "tiger.mp3",
+							"effect": "tiger.mp3",
+							"picture": "tiger.jpg"
 						}
 					]
 				},
@@ -37,18 +103,24 @@ var mediaMapJson = {
 							"picture": "bee.jpg"
 						},
 						{
-							"word": "airplane",
-							"spoken": "airplane.mp3",
+							"word": "avião",
+							"spoken": "aviao.mp3",
 							"effect": "airplane.mp3",
 							"picture": "airplane.jpg"
 						}
 					],
 					"b": [
 						{
-							"word": "bebe",
+							"word": "bebê",
 							"spoken": "baby.mp3",
 							"effect": "baby.mp3",
 							"picture": "baby.jpg"
+						},
+						{
+							"word": "bola",
+							"spoken": "bola.mp3",
+							"effect": "ball.mp3",
+							"picture": "ball.jpg"
 						}
 					],
 					"c": [
@@ -59,6 +131,14 @@ var mediaMapJson = {
 							"picture": "car.jpg"
 						}
 					],
+					"d": [
+						{
+							"word": "dinossauro",
+							"spoken": "dinossauro.mp3",
+							"effect": "dinossaur.mp3",
+							"picture": "dinossaur.jpg"
+						}
+					],
 					"e": [
 						{
 							"word": "elefante",
@@ -66,7 +146,31 @@ var mediaMapJson = {
 							"effect": "elephant.mp3",
 							"picture": "elephant.jpg"
 						}
-					]
+					],
+					"l": [
+						{
+							"word": "leão",
+							"spoken": "leao.mp3",
+							"effect": "lion.mp3",
+							"picture": "lion.jpg"
+						}
+					],
+					"u": [
+						{
+							"word": "urso",
+							"spoken": "urso.mp3",
+							"effect": "bear.mp3",
+							"picture": "bear.jpg"
+						}
+					],
+					"t": [
+					{
+						"word": "tigre",
+						"spoken": "tigre.mp3",
+						"effect": "tiger.mp3",
+						"picture": "tiger.jpg"
+					}
+				]
 				},
 				"marc": {},
 				"bruna": {}
